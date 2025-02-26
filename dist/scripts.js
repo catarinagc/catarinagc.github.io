1580e632d03d6b9223673e329f82012eaeba65d0
catarinagc <catarinagcostaa@gmail.com>
2025-02-26 15:41:06 +0000

    some general changes in aspect

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,6 +1,9 @@
 function projButtonOnClick(buttonClicked){
     console.log(buttonClicked.id)
-
+    const allButtons = document.querySelectorAll("#projectsTab button");
+    allButtons.forEach(button => button.classList.remove("active-tab"));
+    buttonClicked.classList.add("active-tab");
+    console.log(buttonClicked.classList)
     var projDiv = Array.from(document.getElementById("projectsDiv").children)
     projDiv.forEach(project => {
         if (!project.classList.contains(buttonClicked.id) && buttonClicked.id != "all" ){
